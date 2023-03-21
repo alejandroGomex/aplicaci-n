cb5bae2cb70153b26a5bdb617c5f94183cef9351
Alejandro <Alejandro.gomezg@autonoma.edu.co>
2023-03-21 11:34:22 -0500

    a

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const personBillRouter = require('./user_schema.router');
+const user_schema_router = require('../models/user_schema');
 function routerApi(app) {
   const router = express.Router();
   /*Endpoint estático http://localhost:5000/api/v1 */
